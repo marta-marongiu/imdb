f1413ca520814ef34fd8fa2728790765ace7a8a4
DIR\marta.marongiu <martamarongiu7@gmail.com>
2016-12-02 18:04:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// //js file imdb
+// $(".single-item").slick({
+//                 dots: true,
+//                 infinite: true,
+//                 centerMode: true,
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             });
